2a4a9152f238294aa53d5c0b8885247608026e29
Ramesh <rameshramesh4426@gmail.com>
2024-02-21 15:50:46 +0530

    clock

---
+++ asynchronous.js
@@ -0,0 +1,12 @@
+function sysn(){
+    console.log('Ramesh')
+    console.log('Raja')
+    console.log('perumal')
+}
+sysn()
+
+setTimeout(()=> console.log('Ramesh'),5000)
+setTimeout(()=> console.log('Raja'),3000)
+setTimeout(()=> console.log('perumal'),1000)
+
+// setInterval(()=> console.log('Ramesh'),5000)
